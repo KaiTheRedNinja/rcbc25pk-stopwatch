bf98ce2b686c0dbb2d4a83d48335917f1291a36a
KaiTheRedNinja <88234730+KaiTheRedNinja@users.noreply.github.com>
2025-10-02 17:11:46 +0800

    fix base path

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  basePath: "/rcbc25pk-stopwatch",
   output: "export",  // <=== enables static exports
   reactStrictMode: true,
 };
